ac2a21d5436e764a1e4c850a45f0a17684de4864
Mirv <reski.raptor@gmail.com>
2018-01-08 15:11:26 +0000

    revised test to reflect 1st refactor

--- StringAling/test/StringAling_test.rb
+++ StringAling/test/StringAling_test.rb
@@ -2,36 +2,41 @@ require "test_helper"
 
 class StringAlingTest < Minitest::Test
   extend StringAling
-    def setup 
-      @theClass = StringAling::BaseString.new
-    end
-  
-    def test_that_it_has_a_version_number
-      refute_nil ::StringAling::VERSION
-    end
+  def setup 
+    @theClass = StringAling::BaseString.new
+  end
 
-    def test_class_loads
-      assert @theClass.class.name, "BaseString"
-    end
-    
-    def test_spam_default_empahasis 
-      assert_match /^Trash everywhere[!.?]$/, @theClass.tons_of_spam(".")
-    end
-    
-    def test_spam_overwriting_empahasis 
-      assert_match /^Trash everywhere[!.?]$/, @theClass.tons_of_spam("!")
-    end
-    
-    def test_spam_default_empahasis 
-      refute_match /[!.?]$/, @theClass.tons_of_spam("")
-    end
-    
-    def test_string_length_increases
-      @more_greetings = StringAling::MoreGreetings.new
-      assert @theClass.greeting.length <  @more_greetings.greet_animals.length
-    end
-    
-    def test_BaseString_class_method
-      assert StringAling::BaseString.welcome, "hi hi hello welcome all"
-    end
+  def test_that_it_has_a_version_number
+    refute_nil ::StringAling::VERSION
+  end
+
+  def test_class_loads
+    assert @theClass.class.name, "BaseString"
+  end
+  
+  def test_class_variable
+    assert @theClass.class_punctuation, "."
+  end
+  
+  def test_string_length_increases
+    @more_greetings = StringAling::MoreGreetings.new
+    assert @theClass.greeting.length <  @more_greetings.greet_animals.length
+  end
+  
+  def test_BaseString_class_method
+    assert StringAling::BaseString.welcome, "hi hi hello welcome all"
+  end
 end
+
+
+  # def test_spam_default_empahasis 
+  #   assert_match /^Trash everywhere[!.?]$/, @theClass.tons_of_spam(".")
+  # end
+  
+  # def test_spam_overwriting_empahasis 
+  #   assert_match /^Trash everywhere[!.?]$/, @theClass.tons_of_spam("!")
+  # end
+  
+  # def test_spam_default_empahasis 
+  #   refute_match /[!.?]$/, @theClass.tons_of_spam("")
+  # end
